ab79cfc0563e25138119992c4119d4eaf576fd29
DuongDucManh <manhmanhd0402@gmail.com>
2025-02-16 13:58:07 +0700

    Update camera manager for player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,8 @@
 using spaceExplorer.DamageSystem;
-using spaceExplorer.Health;
+using System.Linq;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace spaceExplorer.Player
 {
@@ -10,21 +12,50 @@ namespace spaceExplorer.Player
         private PlayerMove playerMove;
         private PlayerAttack playerAttack;
         private readonly float damage = 10f;
-
+        
         private void Awake()
         {
             if(Instance == null)
             {
                 Instance = this;
+                DontDestroyOnLoad(gameObject); // Keep player persistent
             }
-
+            else
+            {
+                Destroy(gameObject); // Destroy duplicate instances
+            }
+            
             playerAttack = GetComponent<PlayerAttack>();
             playerAttack.DamageDealer = GetComponent<DamageDealer>();
             playerMove = GetComponent<PlayerMove>();
             playerMove.PlayerTransform = transform;
+
+            transform.position = Vector3.zero; // Reset player position
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        private void Start()
+        {
+            
+            CinemachineCameraManager.Instance.SetFollowTarget(transform);
+        }
+        private void Update()
+        {
+            if (SceneManager.GetActiveScene().name.Equals("EndMenu"))
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+        }
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            CinemachineCameraManager.Instance.SetFollowTarget(transform);
+            
         }
         float IDamageSource.GetDamage() => damage;
-        
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
     }
 }
 
